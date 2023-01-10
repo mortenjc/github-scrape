46ee198c6e1f53e78fa319e898f594ca4a5e4829
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-01-10 22:19:58 +0100

    unknown state

--- scrape.py
+++ scrape.py
@@ -1,51 +1,85 @@
 from github import Github
 from pprint import pprint
 import pandas as pd
-import json
+import json, argparse
+
+def addtag(tag, text):
+    return '<' + tag + '>' + text + '</' + tag + '>\n'
+
+def maketable(headers, entries):
+    table = '<table id="repos">'
+    table = table + '<tr>'
+    for header in headers:
+        table = table + addtag('th', header)
+    table = table + '</tr>'
+    return table
+
 
 def get_repo_dict(repo):
-    return {"Full name": repo.full_name,
-            "Description": repo.description,
-            "Date created": repo.created_at,
-            "Date of last push": repo.pushed_at,
-            "Home Page": repo.homepage,
-            "Language": repo.language,
-            "Number of forks": repo.forks,
-            "Number of stars": repo.stargazers_count,
-            "Topics": repo.get_topics(),
-            "Labels": [i._rawData for i in repo.get_labels()],
-            "Contributors": [i._rawData for i in repo.get_contributors()],
-            "Contributors Count": repo.get_contributors().totalCount,
-            #"Subscribers": [i._rawData for i in repo.get_subscribers()],
-            "Subscribers Count": repo.get_subscribers().totalCount,
-            #"Watchers": [i._rawData for i in repo.get_watchers()],
-            "Watchers Count": repo.get_watchers().totalCount
+    return {"name": repo.full_name,
+            "description": repo.description,
+            "created": repo.created_at,
+            "pushdate": repo.pushed_at,
+            "homrpage": repo.homepage,
+            "language": repo.language,
+            "nforks": repo.forks,
+            "nstars": repo.stargazers_count,
+            "topics": repo.get_topics(),
+            "labels": [i._rawData for i in repo.get_labels()],
+            "contributors": [i._rawData for i in repo.get_contributors()],
+            "ncontributors": repo.get_contributors().totalCount,
+            "nsubscribers": repo.get_subscribers().totalCount,
+            "nwatchers": repo.get_watchers().totalCount,
+            "nissues" : repo.get_issues().totalCount,
+            "nbranches" :repo.get_branches().totalCount
            }
 
-repos = ["event-formation-unit", "h5cpp", "forwarder", "daqlite", "efu-legacy-modules"]
-
-g = Github("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")
-
-def report(reponame):
+def report(g, reponame):
     fullreponame = "ess-dmsc/" + reponame
     repo = g.get_repo(fullreponame)
     dict = get_repo_dict(repo)
-    issues = repo.get_issues()
 
-    print("{:25} {:5} {:5} {:13} {:11} {:4}".format(
-        reponame, dict["Number of forks"], dict["Number of stars"],\
-        dict["Contributors Count"], dict["Subscribers Count"], issues.totalCount))
-
-print("{:25} {:4} {:4} {:4} {:4} {:4}".format(
-    "Repository", "forks", "stars", "contributors", "subscribers", "issues"))
-
-for reponame in repos:
-    report(reponame)
+    print("{:25} {:5} {:5} {:13} {:11} {:4} {:4}".format(
+        reponame, dict["nforks"], dict["nstars"],\
+        dict["ncontributors"], dict["nsubscribers"],
+        dict["nissues"], dict["nbranches"]))
 
 
-# # g = Github("<Github user key>") # insert key for a higher rate limit
-# TOPIC = "interview-practice"
+def getrepos(g, user):
+    user = g.get_user(user)
+    repos = user.get_repos()
+
+    non_forks = []
+    for repo in user.get_repos():
+        if repo.fork is False:
+            non_forks.append(repo.name)
+
+    return non_forks
+
+
+def scrape(token):
+    repos = ["event-formation-unit" , "h5cpp", "daquiri", "daqlite", "efu-legacy-modules"]
+    g = Github(token)
+
+    #repos = getrepos(g, 'ess-dmsc')
+
+    print("{:27} {:4} {:4} {:4} {:4} {:4} {:4}".format(
+        "repository", "forks", "stars", "contributors", "subscribers", "issues", "branches"))
+
+    for reponame in repos:
+        report(g, reponame)
+
+
 #
-# # search by topic
-# all_repo = g.search_repositories(f'topic:{TOPIC}')
-# print(all_repo.totalCount) # returns 678 repositories
+#
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", metavar="token", help="github token", type=str, default="")
+    args = parser.parse_args()
+
+
+    #headers = ['a', 'b', 'c']
+    #print(maketable(headers, headers))
+
+    scrape(args.t)
